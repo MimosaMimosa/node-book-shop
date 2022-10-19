1c6efa3bd0f4dc7b99cb1f4fca136985a892fd46
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2022-10-20 03:54:49 +0630

    fixed buged

--- controller/category.js
+++ controller/category.js
@@ -3,7 +3,7 @@ const Category = require('../model/Category');
 exports.create = async (req, res, next) => {
     try {
         const category = new Category(req.body);
-        category.save();
+        await category.save();
         return res.status(200).json(category);
     } catch (error) {
         next(error);
