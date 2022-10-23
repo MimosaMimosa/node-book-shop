40d9a5fafdc50c24b68c1b49f68c0765c3c09704
MimosaMimosa <waiyanlin2991998@gmail.com>
2022-10-24 01:49:23 +0630

    change user moda

--- model/User.js
+++ model/User.js
@@ -4,10 +4,12 @@ const { Schema } = mongoose;
 const Collection = new Schema(
     {
         name: { type: String, required: true, max: 50 },
+        name_and_surname: { type: String, required: true, max: 50 },
         email: { type: String, required: true, unique: true, max: 5 },
         role: { type: Number, default: 1 },
         password: { type: String, required: true, max: 50 },
-        Address: { type: String, required: true, max: 50 },
+        address: { type: String, required: true, max: 50 },
+        country: { type: String, required: true, max: 50 },
     },
     { timestamps: true }
 );
