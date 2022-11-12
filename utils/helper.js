55609cd3c5df304eca72b3bbe046ca94d8f69632
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2022-11-12 08:27:48 +0630

    pagination helper

--- utils/helper.js
+++ utils/helper.js
@@ -1,9 +1,16 @@
+/**
+ * 
+ * @param {Object} req 
+ * @param {number} amount pagination limit
+ * @returns 
+ */
 exports.calculatePaginate = function (req, amount = 10) {
 	const currentPage = parseInt(req.query.page ?? 1);
 	const limit = parseInt(req.query.limit ?? amount);
 	const skip = limit * (currentPage - 1);
 	return [currentPage, skip, limit];
 };
+
 exports.config = (search, df = null) => {
 	try {
 		const [file, key] = search;
@@ -13,49 +20,45 @@ exports.config = (search, df = null) => {
 		console.error(error);
 	}
 };
-exports.usePagination = (total, current) => {
+
+/**
+ * Auto generate pagination for client
+ * @param {number} total
+ * @param {number} current
+ * @param {string}  spreader
+ * @returns {Array} [1,...,3,4,5...100],[1...97,98,99,100],[1,2,3,4,7,8]
+ */
+exports.usePagination = (total, current, spreader = "...") => {
 	let pagination = [];
 	if (total >= 8) {
 		if (current < 5) {
 			pagination = [
-				...[...Array(5).keys()].map((x) => x + 1),
-				"...",
+				...this.range(5).map((x, y) => x + y),
+				spreader,
 				total,
 			];
 		} else if (current >= 5 && current + 3 < total) {
 			pagination = [
 				1,
-				"...",
-				...Array(3)
-					.fill(current - 1)
-					.map((x, y) => x + y),
-				"...",
+				spreader,
+				...this.range(3, current - 1),
+				spreader,
 				total,
 			];
 		} else if (current + 3 >= total && current !== 5) {
-			pagination = [
-				1,
-				"...",
-				...Array(5)
-					.fill(total - 4)
-					.map((x, y) => x + y),
-			];
+			pagination = [1, spreader, ...this.range(5, total - 4)];
 		}
 	} else {
-		pagination = [
-			...Array(total)
-				.fill(1)
-				.map((x, y) => x + y),
-		];
+		pagination = this.range(total, 1);
 	}
 
 	return pagination;
 };
 
 /**
- * 
- * @param {number} start 
- * @param {number} end 
+ *
+ * @param {number} start
+ * @param {number} end
  * @returns {Array}
  */
 exports.range = (start, end) => {
